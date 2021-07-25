cb5ea828ea6648f6db823456b012ec0457a80a95
Josep Miquel <tommyshelby@disroot.org>
2021-07-25 13:59:08 +0200

    Working with minimums

--- classes/PlayMatches.js
+++ classes/PlayMatches.js
@@ -2,15 +2,58 @@ export default class PlayMatches {
     constructor(matches) {
         this.matches = matches;
         this.winers = [];
+        this.rounds = 0;
+        this.results = [];
     }
 
     play() {
-        let newMatch = []
+        let newMatch = [];
+        let matchIndex = 1;
+        // Vaciamos los ganadores y resultados cada vez que se inicia una nueva ronda
+        this.winers = [];
+        this.results = [];
+        this.rounds++;
 
+
+        function announcement(rounds) {
+            switch (rounds) {
+                case 1:
+                    console.log("\n===== OCTAVOS DE FINAL =====")
+                    break;
+                case 2:
+                    console.log("\n===== CUARTOS DE FINAL =====");
+                    break;
+                case 3:
+                    console.log("\n===== SEMIFINALES =====");
+                    break;
+                case 4:
+                    console.log("\n===== FINAL =====")
+                    break;
+            }
+        }
+
+        // Funcion que crea valor aleatorio del 1 al 10
         function goals() {
             return Math.round(Math.random() * 10);
         }
 
+        // En caso de empate
+        function penaltis(team) {
+            let matchPenalti = {
+                'team1': goals(),
+                'team2': goals()
+            }
+
+            if (matchPenalti.team1 > matchPenalti.team2) {
+                newMatch.push(team[0]);
+            } else if (matchPenalti.team1 === matchPenalti.team2) {
+                penaltis(team);
+            } else {
+                newMatch.push(team[1]);
+            }
+        }
+
+        // Recorre cada partido
         this.matches.forEach(team => {
             let match = {
                 'team1': goals(),
@@ -19,6 +62,8 @@ export default class PlayMatches {
 
             if (match.team1 > match.team2) {
                 newMatch.push(team[0]);
+            } else if (match.team1 === match.team2) {
+                penaltis(team);
             } else {
                 newMatch.push(team[1]);
             }
@@ -28,13 +73,28 @@ export default class PlayMatches {
                 newMatch = [];
             }
 
+            // TODO cuando empatan no tiene que mostrar este resultado sino el de los penaltis
+            this.results.push(`${team[0]} ${match.team1} - ${team[1]} ${match.team2} \t\t (Q${matchIndex})`);
+
+            matchIndex++;
+
+
         });
 
+
         // Se juega la final por lo tanto anunciamos el ganador
-        if (this.matches.length === 1) {
-            return newMatch;
+        if (this.rounds == 4) {
+            announcement(this.rounds);
+            this.results.forEach(result => console.log(result));
+            console.log(`===============================================\n${newMatch[0].toUpperCase()} campeona de la EURO!\n===============================================`);
+            return;
         }
 
-        return this.winers;
+        announcement(this.rounds);
+        this.results.forEach(result => console.log(result));
+        // Le pasamos los ganadores a matches para preparar la nueva ronda
+        this.matches = this.winers;
+        // Llamamos a la funcion de manera recursiva
+        this.play();
     }
 }
--- index.js
+++ index.js
@@ -3,35 +3,12 @@ import CreateTournament from "./classes/CreateTournament.js";
 import TableTournament from "./classes/TableTournament.js";
 import PlayMatches from "./classes/PlayMatches.js";
 
-
+// Se crea el torneo
 const torneo = new CreateTournament(teams);
 
-// 16vos
+// Se mezclan los equipos para la primera ronda
 const teamsOfTheTournament = torneo.createMatches();
-console.log("---- DECISEISAVOS DE FINAL----\n");
-console.log(teamsOfTheTournament);
 
-// Quien pasa a Octavos?
-const toOctavos = new PlayMatches(teamsOfTheTournament).play();
-console.log("---- OCTAVOS DE FINAL----\n");
-console.log(toOctavos);
-
-
-// Quien pasa a Semis?
-const secondRound = new PlayMatches(toOctavos).play();
-console.log("---- SEMIFINALES ----\n");
-console.log(secondRound);
-
-// Quien juega la final?
-const finalRound = new PlayMatches(secondRound).play();
-console.log("---- FINAL ----\n");
-console.log(finalRound);
-
-// Ganador del torneo
-const winerAnnouncement = new PlayMatches(finalRound).play();
-console.log(`
-********
-* EL GANADOR DEL TORNEO ES: ${winerAnnouncement[0].toUpperCase()}!!!!!
-********
-`
-);
+// Se juega el torneo
+console.log("===============================================\n==== COMIENZO DE LA FASE DE ELIMINATORIAS =====\n===============================================\n");
+new PlayMatches(teamsOfTheTournament).play();
